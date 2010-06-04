5ef3e9b69ce18926e276e3a82ddf8b49b2b040b4
Felipe Elias Philipp <felipeelias@gmail.com>
2010-06-04 18:25:59 -0300

    connect with pin

--- connect.rb
+++ connect.rb
@@ -9,17 +9,17 @@ oauth = Twitter::OAuth.new(config['token'], config['secret'])
 if config['atoken'] && config['asecret']
   oauth.authorize_from_access(config['atoken'], config['asecret'])
   twitter = Twitter::Base.new(oauth)
-  pp twitter.friends_timeline
+  pp twitter.friends.map { |friend| friend.screen_name }.join(', ')
 
-elsif config['rtoken'] && config['rsecret']
-  oauth.authorize_from_request(config['rtoken'], config['rsecret'], 'PIN')
-  twitter = Twitter::Base.new(oauth)
-  pp twitter.friends_timeline
+elsif config['rtoken'] && config['rsecret'] && config['pin']
+  oauth.authorize_from_request(config['rtoken'], config['rsecret'], config['pin'])
 
   config.update({
     'atoken'  => oauth.access_token.token,
     'asecret' => oauth.access_token.secret,
-  }).delete('rtoken', 'rsecret')
+  }).delete('rtoken', 'rsecret', 'pin')
+  
+  puts "> Now you're authorized!"
 else
   config.update({
     'rtoken'  => oauth.request_token.token,
@@ -28,4 +28,9 @@ else
 
   # authorize in browser
   %x(open #{oauth.request_token.authorize_url})
+
+  print "> what was the PIN twitter provided you with? "
+  pin = gets.chomp
+  
+  config.update({'pin' => pin}) if !pin.empty?
 end
