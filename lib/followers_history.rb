4a9fed8995ce4d26631e7d19d8b389b83aea7341
Felipe Elias Philipp <felipeelias@gmail.com>
2010-07-14 23:22:49 -0300

    ideas for methods

--- lib/followers_history.rb
+++ lib/followers_history.rb
@@ -1,3 +1,7 @@
+# TODO: possible methods
+# FollowersHistory.all (loads all files from dump ordered by date desc)
+# FollowersHistory.new(date, followers)
+# FollowersHistory.new.diff another
 class FollowersHistory
   
 end
