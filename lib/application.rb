d37cbae27dc7a8214b6fd12ee2b155bd0fe537a6
Felipe Elias Philipp <felipeelias@gmail.com>
2010-07-26 20:09:03 -0300

    Application module, with constants and configuration

---
+++ lib/application.rb
@@ -0,0 +1,29 @@
+module Application
+  @@environment = 'development'
+  
+  class << self
+    
+    def root
+      Pathname.new(File.expand_path(File.join(File.dirname(__FILE__), '..')))
+    end
+    
+    def db_root
+      Pathname.new(File.join(root, 'db', "#{env}.yml"))
+    end
+    
+    def env
+      @@environment
+    end
+    
+    def env=(environment)
+      @@environment = environment
+    end
+    
+    [:test, :development, :production].each do |env|
+      define_method "#{env}?" do
+        @@environment == env.to_s
+      end
+    end
+    
+  end
+end
