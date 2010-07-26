04b15c23d8f96f1c8bc4f87a4f6d9a4ae6569c32
Felipe Elias Philipp <felipeelias@gmail.com>
2010-07-26 17:05:59 -0300

    initializer load all files

--- lib/initializer.rb
+++ lib/initializer.rb
@@ -1,2 +1,5 @@
 ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..')) unless defined?(ROOT)
 DB_FILE = File.join(ROOT, 'db', 'data.yml')
+
+require "#{ROOT}/lib/followers_store"
+require "#{ROOT}/lib/followers_history"
