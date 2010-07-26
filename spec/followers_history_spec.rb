1e6a66302a7b6052731b95a3dc822991648a87b2
Felipe Elias Philipp <felipeelias@gmail.com>
2010-07-26 11:56:42 -0300

    refactoring tests

--- spec/followers_history_spec.rb
+++ spec/followers_history_spec.rb
@@ -3,17 +3,20 @@ require 'spec_helper'
 describe FollowersHistory, ".new" do
   subject { FollowersHistory.new([111, 222]) }
   
-  it "should have followers" do
+  it do
     subject.should have(2).followers
+  end
+  
+  it do
     subject.followers.should include(111, 222)
   end
 end
   
 describe FollowersHistory, "comparing histories" do
   let(:old_history) { FollowersHistory.new([111, 222, 333]) }
-  let(:new_history) { FollowersHistory.new([111]) }
 
   it "should show the ids of the unfollowers" do
+    new_history = FollowersHistory.new([111])
     new_history.diff(old_history).should == [222, 333]
   end
   
@@ -24,8 +27,11 @@ describe FollowersHistory, "comparing histories" do
 end
 
 describe FollowersHistory, "loading all history" do
-  it "should load history" do
+  before do
     FollowersHistory.store = FollowersStore.new("#{ROOT}/spec/fixtures/simple_dump.yml")
+  end
+  
+  it "should load history" do
     history = FollowersHistory.all
 
     history.first.followers.should == [1, 2]
@@ -38,7 +44,5 @@ describe FollowersHistory, "saving a new history" do
     FollowersHistory.store = FollowersStore.new("#{ROOT}/spec/fixtures/save_dump.yml")
   end
   
-  it "should add a new history if different" do
-    
-  end
+  it "should add a new history if different"
 end
