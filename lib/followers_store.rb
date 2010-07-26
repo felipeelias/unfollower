c6058bc9c51ab6f6153d04d117e0f15b2089bcf2
Felipe Elias Philipp <felipeelias@gmail.com>
2010-07-26 10:58:00 -0300

    follower store using yaml

--- lib/followers_store.rb
+++ lib/followers_store.rb
@@ -1,9 +1,34 @@
-module FollowersStore
-  @@db_file = DB_FILE
+class FollowersStore
+  attr_reader :followers
   
-  class << self
-    def db_file
-      @@db_file
+  def initialize(file)
+    @file = file
+    load
+  end
+  
+  def add(follower)
+    @followers << follower
+  end
+  
+  def count
+    @followers.size
+  end
+  
+  def dump!
+    File.open(@file, "w") { |f| f.write(to_array) }
+    load
+  end
+  
+  def to_array
+    @followers.map {|follower| follower.followers }
+  end
+  
+  private
+  
+  def load
+    @followers = YAML.load(File.read(@file)).map do |history|
+      FollowersHistory.new(history)
     end
   end
+  
 end
--- spec/followers_store_spec.rb
+++ spec/followers_store_spec.rb
@@ -1,7 +1,33 @@
 require 'spec_helper'
 
 describe FollowersStore do
-  it "should add a new follower"
-  it "should load all followers from file"
-  it "should dump the followers"
+  subject { FollowersStore.new("#{ROOT}/spec/fixtures/simple_dump.yml") }
+  
+  it do
+    should have(2).followers
+  end
+  
+  it "should add a new follower" do
+    follower = FollowersHistory.new([1, 2])
+    
+    expect { subject.add follower }.to change { subject.count }.from(2).to(3)
+  end
+  
+  it "should load all followers from file" do
+    followers = subject.followers
+    followers.should have(2).itens
+    followers.first.should be_an_instance_of(FollowersHistory)
+  end
+  
+  it "should dump correctly the followers" do
+    File.should_receive(:open).with("#{ROOT}/spec/fixtures/simple_dump.yml", "w")
+    subject.should_receive(:load)
+    subject.dump!
+  end
+  
+  it "should convert to array" do
+    followers_to_array = subject.to_array
+    followers_to_array.should be_an_instance_of(Array)
+    followers_to_array.first.should be_an_instance_of(Array)
+  end
 end
