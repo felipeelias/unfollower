028142c105e0d5af72eb8d6f8db3e0a368e16846
Felipe Elias Philipp <felipeelias@gmail.com>
2010-07-28 16:25:43 -0300

    little refactor and corrected bug in specs

--- lib/application/twitter_config.rb
+++ lib/application/twitter_config.rb
@@ -2,9 +2,10 @@ require 'yaml'
 
 module Application
   class TwitterConfig < Struct.new(:token, :secret, :callback)
-    def initialize(file, environment = 'development')
-      @config = YAML.load(File.read(file))[environment]
-      super(@config['consumer_token'], @config['consumer_secret'], @config['oauth_callback'])
+    def initialize(environment = Application.env)
+      yaml = YAML.load(File.read("#{Application.root}/twitter.yml"))
+      config = yaml[environment]
+      super(config['consumer_token'], config['consumer_secret'], config['oauth_callback'])
     end
   end
 end
--- spec/twitter_config_spec.rb
+++ spec/twitter_config_spec.rb
@@ -1,45 +1,20 @@
 require 'spec_helper'
 
-shared_examples_for 'twitter config' do
+describe Application::TwitterConfig do
+
   it { should respond_to(:token) }
   it { should respond_to(:secret) }
   it { should respond_to(:callback) }  
-end
-
-describe Application::TwitterConfig do
-  subject { Application::TwitterConfig.new("#{Application.root}/spec/fixtures/sample_twitter_config.yml") }
   
-  it_should_behave_like "twitter config"
-  
-  context "in default environment" do
-    it "should have development token" do
-      subject.token == "DEV_TOKEN"
-    end
-    
-    it "should have development secret" do
-      subject.secret == "DEV_SECRET"
-    end
-    
-    it "should have development callback" do
-      subject.callback == "http://dev_callback"
-    end
-  end
-end
-
-describe Application::TwitterConfig, "in another environment" do
-  subject { Application::TwitterConfig.new("#{Application.root}/spec/fixtures/sample_twitter_config.yml", 'production') }
-
-  it_should_behave_like "twitter config"
-
-  it "should have production token" do
-    subject.token == "PROD_TOKEN"
+  it "should have token set" do
+    subject.token.should == "TEST_TOKEN"
   end
   
-  it "should have production secret" do
-    subject.secret == "PROD_SECRET"
+  it "should have secret set" do
+    subject.secret.should == "TEST_SECRET"
   end
   
-  it "should have production callback" do
-    subject.callback == "http://prod_callback"
+  it "should have callback set" do
+    subject.callback.should == "http://test_callback"
   end
 end
