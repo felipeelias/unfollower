3b4b73d754ebd0b7b58b156d52eeb132a0331cbb
Felipe Elias Philipp <felipeelias@gmail.com>
2010-09-06 21:26:48 +0100

    App.root desnecessario

--- lib/initializer.rb
+++ lib/initializer.rb
@@ -1,3 +1,3 @@
-require File.expand_path(File.join(File.dirname(__FILE__), 'application'))
-require "#{Application.root}/lib/followers_store"
-require "#{Application.root}/lib/followers_history"
+require './lib/application'
+require "./lib/followers_store"
+require "./lib/followers_history"
