d2c0a7f7fa24414a80b3f09115691383aa2e9ceb
Felipe Elias Philipp <felipeelias@gmail.com>
2010-09-11 19:17:28 +0100

    sinatra in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,13 @@
+require 'sinatra'
+require 'rack/test'
+
 require File.expand_path(File.dirname(__FILE__) + '/../lib/initializer')
+
+# set test environment
+set :environment, :test
+set :run, false
+set :raise_errors, true
+set :logging, false
 
 Application.env = 'test'
 
