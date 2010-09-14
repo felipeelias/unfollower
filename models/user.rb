3fafcaf212a6417d82de0cd815de3490b06bebb5
Felipe Elias Philipp <felipeelias@gmail.com>
2010-09-14 22:15:42 +0100

    handling unfollowers in user model

--- models/user.rb
+++ models/user.rb
@@ -2,4 +2,11 @@ class User
   include MongoMapper::Document
   
   key :twitter_id, Integer, :required => true
+  key :last_followers, Array
+  key :unfollowers, Array
+  
+  def check_unfollowers(new_list)
+    list = last_followers - new_list
+    unfollowers.unshift(*list)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,8 +2,30 @@ require 'spec_helper'
 
 describe User do
   it { should respond_to(:twitter_id) }
+  it { should respond_to(:last_followers) }
   
   it "should not create a invalid user" do
     User.new.should_not be_valid
+  end
+  
+  it "should have default empty array for unfollowers" do
+    user = User.create(:twitter_id => 0, :last_followers => [1])
+    user.unfollowers.should be_empty
+  end
+end
+
+describe User, "handling unfollowers" do
+  let(:user) { User.create(:twitter_id => 0, :last_followers => [1, 2, 3], :unfollowers => [4]) }
+  
+  it "should update the unfollowers list if have changes" do
+    new_followrers_list = [2, 3]
+    user.check_unfollowers(new_followrers_list)
+    user.unfollowers.should == [1, 4]
+  end
+  
+  it "should not update the unfollowers list if does not have changes" do
+    same_followrers_list = [1, 2, 3]
+    user.check_unfollowers(same_followrers_list)
+    user.unfollowers.should == [4]
   end
 end
