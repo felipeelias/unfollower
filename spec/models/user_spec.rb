7c7c8cdb57e7c5ae7b5016c496220d988dce2f43
Felipe Elias Philipp <felipeelias@gmail.com>
2010-11-15 00:20:11 +0000

    using expect/change blocks

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,26 +15,37 @@ describe User do
 end
 
 describe User, "handling unfollowers" do
-  let(:user) { User.create(:twitter_id => 0, :last_followers => [1, 2, 3], :unfollowers => [4]) }
+  before do 
+    @user = User.create(:twitter_id => 0, :last_followers => [1, 2, 3], :unfollowers => [4])
+  end
   
   it "should update the unfollowers list if have changes" do
     new_followers_list = [2, 3]
-    user.check_unfollowers!(new_followers_list)
-    
-    User.find_by_twitter_id(0).unfollowers.should == [1, 4]
+
+    expect { 
+      @user.check_unfollowers!(new_followers_list) 
+    }.to change {
+      User.find_by_twitter_id(0).unfollowers
+    }.from([4]).to([1, 4])
   end
   
   it "should update the last_followers if have changes" do
     new_followers_list = [2, 3]
-    user.check_unfollowers!(new_followers_list)
-    
-    User.find_by_twitter_id(0).last_followers.should == [2, 3]
+
+    expect { 
+      @user.check_unfollowers!(new_followers_list) 
+    }.to change {
+      @user.last_followers
+    }.from([1, 2, 3]).to([2, 3])
   end
   
   it "should not update the unfollowers list if does not have changes" do
-    same_followrers_list = [1, 2, 3]
-    user.check_unfollowers!(same_followrers_list)
+    same_followers_list = [1, 2, 3]
     
-    User.find_by_twitter_id(0).unfollowers.should == [4]
+    expect { 
+      @user.check_unfollowers!(same_followers_list) 
+    }.to_not change {
+      User.find_by_twitter_id(0).unfollowers
+    }
   end
 end
