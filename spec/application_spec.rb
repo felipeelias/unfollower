06994846064d4b86019623122d4bdf3debb5314f
Felipe Elias Philipp <felipeelias@gmail.com>
2010-11-15 22:02:10 +0000

    try to mock twitter client

--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -3,25 +3,27 @@ require 'spec_helper'
 describe "Application" do
   include Rack::Test::Methods
 
+  # before do
+  #   oauth = Twitter::OAuth.new("token", "secret")
+  #   @access_token = OAuth::AccessToken.new(oauth.consumer, "atoken", "asecret")
+  #   oauth.stub!(:access_token).and_return(@access_token)
+  #   @twitter = Twitter::Base.new(oauth)
+  # end
+
   def app
-    @app ||= Sinatra::Application
+    Sinatra::Application.new
   end
 
   it "should show index page if logged in" do
     pending
-    
-    oauth = mock(Twitter::OAuth)
-    oauth.should_receive(:authorize_from_access).with("XYZ", "XYZIOP")
-    twitter = mock(Twitter::Base)
-    twitter.should_receive(:new).with(oauth)
-    
-    get '/', {}, {'rack.session' => {:oauth => {:access_token => "XYZ", :access_token_secret => "XYZIOP"}}}
+    get '/', {}, { 'rack.session' => {:current_user => 1} }
     
     last_response.should_not be_redirect
   end
 
   it "should redirect to login if not logged in" do
     get '/'
+    
     last_response.should be_redirect
   end
 end
