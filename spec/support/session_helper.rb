5a251ea036739c95c94135697698d2b2e1aa6c99
Felipe Elias Philipp <felipeelias@gmail.com>
2011-01-03 23:55:55 +0000

    helper files

---
+++ spec/support/session_helper.rb
@@ -0,0 +1,30 @@
+class SessionData
+  def initialize(cookies)
+    @cookies = cookies
+    @data = cookies['rack.session']
+    if @data
+      @data = @data.unpack("m*").first
+      @data = Marshal.load(@data)
+    else
+      @data = {}
+    end
+  end
+  
+  def [](key)
+    @data[key]
+  end
+  
+  def []=(key, value)
+    @data[key] = value
+    session_data = Marshal.dump(@data)
+    session_data = [session_data].pack("m*")
+    @cookies.merge("rack.session=#{Rack::Utils.escape(session_data)}", URI.parse("//example.org//"))
+    raise "session variable not set" unless @cookies['rack.session'] == session_data
+  end
+end
+
+module SessionHelper  
+  def session
+    SessionData.new(rack_test_session.instance_variable_get(:@rack_mock_session).cookie_jar)
+  end
+end
