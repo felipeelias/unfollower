ce989ee5429b929207678ceef3315dc335b0f148
Felipe Elias Philipp <felipeelias@gmail.com>
2011-01-04 00:37:07 +0000

    better spec description

--- spec/application_spec.rb
+++ spec/application_spec.rb
@@ -8,40 +8,53 @@ describe "Application" do
     Sinatra::Application.new
   end
   
-  it "should redirect to the twitter login" do
-    stub_oauth_request_token!
-    get '/request'
-    
-    session[:request_token].should == 't'
-    session[:request_secret].should == 's'
-    
-    last_response.headers['Location'].should =~ /api.twitter.com/
+  it "should redirect to login if not logged in" do
+    get '/'
     last_response.should be_redirect
   end
   
-  it "should receive the auth token" do
-    stub_oauth_access_token!
-    get '/auth', { :oauth_verifier => 'some_key' }
-
-    session[:access_token].should == 'at'
-    session[:access_secret].should == 'as'
+  describe "/request" do
+    before do
+      stub_oauth_request_token!
+    end
     
-    last_response.should be_redirect
+    it "should redirect to the twitter login" do
+      get '/request'
+
+      session[:request_token].should == 't'
+      session[:request_secret].should == 's'
+
+      last_response.headers['Location'].should =~ /api.twitter.com/
+      last_response.should be_redirect
+    end
   end
-
-  it "should show index page if logged in" do
-    session[:access_token] = 'token'
-    session[:access_secret] = 'secret'
-    stub_verify_credentials!
+  
+  describe "/auth" do
+    before do
+      stub_oauth_access_token!
+    end
     
-    get '/'
+    it "should receive the auth token from twitter" do
+      get '/auth', { :oauth_verifier => 'some_key' }
 
-    last_response.should_not be_redirect
+      session[:access_token].should == 'at'
+      session[:access_secret].should == 'as'
+
+      last_response.should be_redirect
+    end
   end
+  
+  describe "/index" do
+    before do
+      session[:access_token] = 'token'
+      session[:access_secret] = 'secret'
+      stub_verify_credentials!
+    end
 
-  it "should redirect to login if not logged in" do
-    get '/'
-    
-    last_response.should be_redirect
+    it "should show index page if logged in" do
+      get '/'
+
+      last_response.should_not be_redirect
+    end
   end
 end
